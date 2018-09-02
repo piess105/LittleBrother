f019ace846959da0a4299eedac38996372a3e4f4
Patryk Wilk <piess105@gmail.com>
2018-09-02 15:31:04 +0200

    First commit

---
+++ LittleBrother.WPF/Converters/LessThenConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LittleBrother.WPF.Converters
+{
+    public class LessThenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (double)value;
+            var par = double.Parse(((string)parameter));
+
+            var res = val < par;
+
+            return res;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
